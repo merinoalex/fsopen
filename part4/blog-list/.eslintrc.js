5157ce089852ea3a172b761e2e01e362e1f58cd3
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-02-14 20:39:10 -0600

    Exercise 4.3

--- part4/blog-list/.eslintrc.js
+++ part4/blog-list/.eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   'env': {
-    'browser': true,
+    'node': true,
     'commonjs': true,
-    'es2021': true
+    'es2021': true,
+    'jest': true,
   },
-  'extends': 'eslint:recommended',
+  'extends': [
+    'eslint:recommended',
+    'plugin:node/recommended'
+  ],
   'overrides': [
   ],
   'parserOptions': {

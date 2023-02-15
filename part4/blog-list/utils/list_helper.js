6ac4413badb3e3120718322ea155b61a84b94c35
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-02-14 21:30:04 -0600

    Exercise 4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (list) => {
+/*   let total = 0
+  for (let blog of list) {
+    total += blog.likes
+  }
+  return total */
+  return list.reduce((a, b) => a + b.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }

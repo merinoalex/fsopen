a500a1c8b0610932fc03c41c3f41e33ebd257c91
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-02-14 18:54:17 -0600

    Exercise 4.2

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Repository for the Full Stack open course 2022 by Alejandro Merino.
 2. [Part 1](https://github.com/merinoalex/fsopen/tree/main/part1)
 3. [Part 2](https://github.com/merinoalex/fsopen/tree/main/part2)
 4. [Part 3](https://github.com/merinoalex/fsopen-part3/)
+5. [Part 4](https://github.com/merinoalex/fsopen/tree/main/part4)
 
 E-mail: merino.aleex@gmail.com
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,65 +1,7 @@
-require('dotenv').config()
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.set('strictQuery', false)
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-const requestLogger = (req, res, next) => {
-  console.log('Method:', req.method)
-  console.log('Path:  ', req.path)
-  console.log('Body:  ', req.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Blog list app</h1>')
-})
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-})
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'Unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })

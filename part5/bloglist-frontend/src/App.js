0b8f1032b5b4e3d4bb5c2b6ec02d9262913a7c11
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-03-22 20:48:32 -0600

    🧪 Exercises 5.2 & 5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,8 +3,51 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const BlogForm = ({ addBlog, newBlogTitle, newBlogAuthor, newBlogUrl, setNewBlogTitle, setNewBlogAuthor, setNewBlogUrl }) => {
+  return (
+    <div>
+      <h2>Add new blog</h2>
+
+      <form onSubmit={addBlog}>
+        <div>
+          Title: 
+          <input
+            type="text"
+            value={newBlogTitle}
+            name="Title"
+            onChange={({ target }) => setNewBlogTitle(target.value)}
+          />
+        </div>
+        <div>
+          Author: 
+          <input 
+            type="text"
+            value={newBlogAuthor}
+            name="Author"
+            onChange={({ target }) => setNewBlogAuthor(target.value)}
+          />
+        </div>
+        <div>
+          URL: 
+          <input 
+            type="text"
+            value={newBlogUrl}
+            name="URL"
+            onChange={({ target }) => setNewBlogUrl(target.value)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
+
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -15,6 +58,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (e) => {
     e.preventDefault()
     
@@ -23,12 +74,41 @@ const App = () => {
         username, password,
       })
 
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('Wrong credentials')
     }
+  }
+
+  const handleLogout = (e) => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    window.location.reload()
+  }
+
+  const addBlog = (e) => {
+    e.preventDefault()
+
+    const blogObject = {
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl
+    }
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewBlogTitle('')
+        setNewBlogAuthor('')
+        setNewBlogUrl('')
+      })
   }
 
   const loginForm = () => (
@@ -64,7 +144,21 @@ const App = () => {
       {!user && loginForm()}
       {user && <div>
           <h2>Blogs</h2>
-          <p>{user.name} is logged in.</p>
+          <p>
+            {user.name} is logged in. 
+            <button onClick={handleLogout}>Logout</button>
+          </p>
+
+          <BlogForm
+            addBlog={addBlog}
+            newBlogTitle={newBlogTitle}
+            newBlogAuthor={newBlogAuthor}
+            newBlogUrl={newBlogUrl}
+            setNewBlogTitle={setNewBlogTitle}
+            setNewBlogAuthor={setNewBlogAuthor}
+            setNewBlogUrl={setNewBlogUrl}
+          />
+
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}

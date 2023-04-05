cf10f5f1dfdd598053ba2803f77bcf0767cc3018
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-04-05 16:39:51 -0600

    🧪 Exercises 5.6-7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    padding: 10,
+    background: '#efefef',
+    margin: 10
+  }
+
+  const detailsStyle = {
+    display: visible ? '' : 'none',
+    paddingLeft: 16
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <strong>{blog.title}</strong> by {blog.author} <button onClick={toggleVisibility}>{visible ? 'Hide' : 'View'}</button>
+      </div>
+      <div style={detailsStyle}>
+        <p>{blog.url}</p>
+        <p>{blog.likes} likes <button>👍</button></p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+)}
 
 export default Blog
